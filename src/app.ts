310942c818740c38d7be88a54a216b2df41dd62a
Miguel Angelo <m.angelodev@gmail.com>
2023-04-26 16:59:45 -0300

    feat: :memo: Adds routes and error handler

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,10 @@
 import express from 'express';
+import ErrorHandler from './Middlewares/ErrorHandler';
+import carRoute from './Routes/CarRoutes';
 
 const app = express();
+app.use(express.json());
+app.use(carRoute);
+app.use(ErrorHandler.handle);
 
 export default app;
